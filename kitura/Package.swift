19379f2bee07c952baa8aab3a810967661f6adc3
David Jones <djones6@uk.ibm.com>
2017-06-20 13:40:32 +0100

    Update to Swift 3.1.1

--- kitura/Package.swift
+++ kitura/Package.swift
@@ -3,8 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Kitura-TechEmpower",
     dependencies: [
-        // Uncomment to restrict Kitura-net to a specific version
-        //.Package(url: "https://github.com/IBM-Swift/Kitura-net.git", Version(1, 1, 0)),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
